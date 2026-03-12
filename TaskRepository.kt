345fb0892e1550806ff9893d93623e74396b35d7
Raghu2424 <raghavendrakshetri@gmail.com>
2026-03-12 17:43:42 +0530

    Add TaskRepository class for task management

---
+++ TaskRepository.kt
@@ -0,0 +1,16 @@
+class TaskRepository(private val taskDao: TaskDao) {
+
+    val allTasks = taskDao.getAllTasks()
+
+    suspend fun insert(task: Task) {
+        taskDao.insert(task)
+    }
+
+    suspend fun update(task: Task) {
+        taskDao.update(task)
+    }
+
+    suspend fun delete(task: Task) {
+        taskDao.delete(task)
+    }
+}
