a58c5cda2605dda8598a54db04124b9a2bbbb506
Raghu2424 <raghavendrakshetri@gmail.com>
2026-03-12 22:15:56 +0530

    Add Task data class for Room database

--- Task.kt
+++ Task.kt
@@ -1,1 +1,13 @@
 
+package com.example.todolistapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_table")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String
+)
